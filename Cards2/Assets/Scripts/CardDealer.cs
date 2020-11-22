ec11fe4acf80a495ef2268678d8a69a330f811a8
AIUENO17 <lallapallooza_1120@au.com>
2020-11-22 16:33:19 +0900

    デッキを配る

---
+++ Cards2/Assets/Scripts/CardDealer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDealer : MonoBehaviour
+{
+    //List<型名> 変数名　= new List<型名>();
+    private List<Card> deck = new List<Card>();
+    //List
+    private List<Card> playerHand = new List<Card>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        deck = Deck.ShuffleDeck(Deck.GetDeck());
+
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+        }
+    }
+}
